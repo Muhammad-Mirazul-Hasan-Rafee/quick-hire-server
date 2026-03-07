23212686ee9433e76a4ddd779d368e8ba8dd344f
Rafee <rafeehasan88@gmail.com>
2026-03-07 11:31:33 +0600

    added get ops to find all jobs

--- index.js
+++ index.js
@@ -27,6 +27,29 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // job related APIs ( List all jobs )
+
+    const jobsCollection = client.db('quichire').collection('jobs');
+    app.get('/jobs' , async (req , res)=>{
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+      
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
