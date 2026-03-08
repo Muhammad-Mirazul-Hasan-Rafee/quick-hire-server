1d13ecffeea80a0ce06a2065c694597b3f7fb823
Rafee <rafeehasan88@gmail.com>
2026-03-08 13:25:52 +0600

    post api for job application done

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhv77.mongodb.net/?appName=Cluster0`;
 
@@ -17,7 +16,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,27 +25,40 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
+     const jobsCollection = client.db("quichire").collection("jobs");
+    const jobApplicationCollection = client.db("quichire").collection("job_applications");
+    const roleCollection = client.db("quichire").collection("users");
     // job related APIs ( List all jobs )
 
-    const jobsCollection = client.db('quichire').collection('jobs');
-    app.get('/jobs' , async (req , res)=>{
+   
+
+    app.get("/jobs", async (req, res) => {
       const cursor = jobsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
     // Get single job details
-    app.get('/jobs/:id' , async(req , res)=>{
+    app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await jobsCollection.findOne(query);
       res.send(result);
     });
 
+    // ..............................Job application APIs...............................
+    // .................................................................................
 
-
+    // Create an application
+    app.post("/job-application", async (req, res) => {
+      const application = req.body;
+     const result = await  jobApplicationCollection.insertOne(application);
+     res.send(result);
+    });
 
 
 
@@ -63,9 +75,11 @@ async function run() {
   }
 }
 run().catch(console.dir);
-app.get('/' , (req , res)=>{
-    res.send('Quick hire is managing!');
+app.get("/", (req, res) => {
+  res.send("Quick hire is managing!");
 });
 app.listen(port, () => {
-  console.log(`Quick Hire is running for job seekers and reqruiters on port: ${port}`);
+  console.log(
+    `Quick Hire is running for job seekers and reqruiters on port: ${port}`
+  );
 });
