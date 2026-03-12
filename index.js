059f8a5f0242fe7dd2ea489189a23ab296d0c3bb
Rafee <rafeehasan88@gmail.com>
2026-03-12 13:58:33 +0600

    return res.status(403).send({message: 'unauthorized accessgit add .'});

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+
+const logger = (req, res, next) => {
+  console.log("Inside logger");
+  next();
+};
+
+const verifyToken = (req , res , next)=>{
+   console.log("Inside vefiry token middle ware");
+   const token = req.cookies?.token;
+   if(!token){
+    return res.status(401).send({message: 'Unauthorized access!'});
+   }
+   jwt.verify(token , process.env.JWT_SEC , (err , decoded)=>{
+    if(err){
+      return res.status(401).send({message:'Unauthorized access!'});
+    }
+    req.user = decoded;
+     next(); 
+
+   })
+
+ 
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhv77.mongodb.net/?appName=Cluster0`;
 
@@ -29,13 +60,33 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
-     const jobsCollection = client.db("quichire").collection("jobs");
-    const jobApplicationCollection = client.db("quichire").collection("job_applications");
+    const jobsCollection = client.db("quichire").collection("jobs");
+    const jobApplicationCollection = client
+      .db("quichire")
+      .collection("job_applications");
     const roleCollection = client.db("quichire").collection("users");
+
+    // Auth related APIs
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SEC, { expiresIn: "1hr" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
     // job related APIs ( List all jobs )
-   
-    app.get("/jobs", async (req, res) => {
-      const cursor = jobsCollection.find();
+    app.get("/jobs", logger  , async (req, res) => {
+      const email = req.query.email;
+
+      let query = {};
+      if (email) {
+        query = { hr_email: email };
+      }
+      const cursor = jobsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -49,7 +100,7 @@ async function run() {
     });
 
     // create job
-    app.post('/jobs' , async(req , res)=>{
+    app.post("/jobs", async (req, res) => {
       const newJob = req.body;
       const result = await jobsCollection.insertOne(newJob);
       res.send(result);
@@ -61,22 +112,25 @@ async function run() {
     // Create an application
     app.post("/job-application", async (req, res) => {
       const application = req.body;
-     const result = await  jobApplicationCollection.insertOne(application);
-     res.send(result);
+      const result = await jobApplicationCollection.insertOne(application);
+      res.send(result);
     });
 
     // Get all application (o to many)
-    app.get('/job-application' , async(req , res)=>{
+    app.get("/job-application", verifyToken , async (req, res) => {
       const email = req.query.email;
-      const query = {applicant_email: email};
+      const query = { applicant_email: email };
+      if(req.user.email !== req.query.email){
+        return res.status(403).send({message: 'unauthorized access!!'});
+      }
+      console.log(req.cookies);
       const result = await jobApplicationCollection.find(query).toArray();
 
-
       // aggregate data
-      for(const application of result){
-        const query1 = {_id: new ObjectId(application.job_id)};
+      for (const application of result) {
+        const query1 = { _id: new ObjectId(application.job_id) };
         const job = await jobsCollection.findOne(query1);
-        if(job){
+        if (job) {
           application.title = job.title;
           application.company = job.company;
           application.company_logo = job.company_logo;
@@ -86,20 +140,17 @@ async function run() {
         }
       }
 
-
       res.send(result);
-
     });
 
     // Delete application
-   
-app.delete("/jobs/:id" , async(req , res)=>{
-  const id = req.params.id;
-  const query = {_id: new ObjectId(id)};
-  const deleteApplication = await jobApplicationCollection.deleteOne(query);
-  res.send(deleteApplication);
-});
 
+    app.delete("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const deleteApplication = await jobApplicationCollection.deleteOne(query);
+      res.send(deleteApplication);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
@@ -114,8 +165,3 @@ app.listen(port, () => {
     `Quick Hire is running for job seekers and reqruiters on port: ${port}`
   );
 });
-
-
-
-
-
